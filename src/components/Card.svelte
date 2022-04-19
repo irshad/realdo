5728ef193da5dd97ff98b1ede0016cc4bb3ce180
Irshad Ali <irshadali786143@gmail.com>
2022-04-19 18:40:09 +0530

    Popup arrow fixed

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -44,6 +44,14 @@
                 <button class="delete-btn br-8" on:click={deleteButton}>
                     Delete
                 </button>
+
+                <span class="menu-arrow">
+                    <svg width="32" height="20" viewBox="-2 -2 37 28" fill="none" xmlns="http://www.w3.org/2000/svg" style="top: -18px; transform: scale(-1, 1); right: 25px;">
+                        <g>
+                            <path d="M0.138981 4.44226C0.0834319 1.0678 3.97439 -0.854559 6.62079 1.23988L35.9246 24.4317C37.4112 25.6083 36.5792 28 34.6834 28L2.49412 28C1.40238 28 0.512363 27.1245 0.494394 26.0329L0.138981 4.44226Z" fill="var(--primary-color)" stroke="#000000" stroke-width="2.5" mask="url(#path-1-inside-1_0_1)" class="arrow-grey"></path>
+                        </g>
+                    </svg>
+                </span>
             </div>
         {/if}
     </div>
@@ -85,7 +93,7 @@
 
     .menu {
         width: 74px;
-        margin-top: 16px;
+        margin-top: 24px;
         border: 2px solid var(--dark-color);
         background-color: var(--primary-color);
         position: absolute;
@@ -93,19 +101,13 @@
         padding: 0 8px 8px 8px;
         z-index: 1111;
         border-radius: 14px;
+        box-shadow: 0px 5px 15px 0px rgba(0,0,0,0.3);
     }
 
-    .menu::before {
-        content: "";
+    .menu-arrow {
+        right: 25px;
+        top: -20px;
         position: absolute;
-        display: block;
-        width: 0;
-        left: 50%;
-        bottom: 94px;
-        border: 9px solid transparent;
-        border-top: 0;
-        border-bottom: 12px solid var(--dark-color);
-        transform: translate(-50%, calc(100% + -4px));
     }
 
     .menu button {
