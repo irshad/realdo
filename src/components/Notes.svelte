24e95925ac1d5ef0f63822590b022012929b2685
Irshad Ali <irshadali786143@gmail.com>
2022-04-24 21:45:17 +0530

    Download & Open file features added

--- src/components/Notes.svelte
+++ src/components/Notes.svelte
@@ -54,7 +54,7 @@
         <div class="main-section" in:fly={{ x:2000, duration: 300 }} out:fly={{ x:3000, duration: 1200 }}>
             <div class="notes-header">
                 <div class="system-menu">
-                    <button class="menu-btn" on:click={() => (openFile = !openFile)}>
+                    <button class="menu-btn" title="Open Downloaded Notes" on:click={() => (openFile = !openFile)}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
                             <polyline points="14 2 14 8 20 8"></polyline>
