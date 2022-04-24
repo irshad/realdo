e452383d163fc265f8769748358688656a9f08af
Irshad Ali <irshadali786143@gmail.com>
2022-04-24 19:34:30 +0530

    In app browser changes

--- src/components/Topbar.svelte
+++ src/components/Topbar.svelte
@@ -4,6 +4,7 @@
     import { createEventDispatcher } from "svelte";
     import Notes from "./Notes.svelte";
     import Toast from "../stores/toast";
+    import { Browser } from '@capacitor/browser';
 
     const dispatch = createEventDispatcher();
 
@@ -26,6 +27,10 @@
 
     const backup = () => {
         Toast.success('Coming Soon!');
+    };
+
+    const contactUs = async () => {
+        await Browser.open({ url: 'https://forms.gle/KYKBrJF3Yhge5TXa6' });
     };
 </script>
 
@@ -71,13 +76,21 @@
     </button>
 
     <button class="nav-button" on:click={backup}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
             <polyline points="16 16 12 12 8 16"></polyline>
             <line x1="12" y1="12" x2="12" y2="21"></line>
             <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
             <polyline points="16 16 12 12 8 16"></polyline>
         </svg>
         Backup
+    </button>
+
+    <button class="nav-button"  on:click={contactUs}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+            <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
+            <polyline points="22,6 12,13 2,6"></polyline>
+        </svg>
+        Contact
     </button>
 </Sidenav>
 
